5bf81fc9793750d60772442cf9fe2a681a5a82db
da2045 <vitde2009@gmail.com>
2025-08-03 14:33:50 +0300

    змінив трохи

--- src/components/unlock_button.tsx
+++ src/components/unlock_button.tsx
@@ -1,7 +1,7 @@
 'use client';
 import React from 'react';
 import Link from 'next/link'
-import { MyButtonProps } from '@/types/types';
+
 
 
 export default function MyButton({ children, className }: MyButtonProps) {
--- src/types/types.tsx
+++ src/types/types.tsx
@@ -1,4 +1,4 @@
-export type MyButtonProps = {
+type MyButtonProps = {
 
   children: React.ReactNode;
   className: string; 

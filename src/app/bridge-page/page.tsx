508aef0d17f629085c5e9e7226a3480f3b753be3
ElBiba <artem.ziber@gmail.com>
2025-08-05 20:53:36 +0300

    new pages

---
+++ src/app/bridge-page/page.tsx
@@ -0,0 +1,49 @@
+import { useRouter } from 'next/navigation'; 
+import styles from './bridge.module.css';
+import React from 'react'
+
+interface ButtonProps {
+  onClick: () => void;
+  Button: React.ReactNode;
+}
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.header}>
+        <h1 className={styles.title}>Bridge</h1>
+      </div>
+      <button className={styles.settings} > 
+          <img src="/settings.png" className={styles.settings_icon} />
+        </button>
+        <button className={styles.back_button}>
+          <img src="/back_button.png" className={styles.back_button_icon} />
+        </button>
+      <div className={styles.rect}>
+      </div>
+      <div className={styles.square}>
+        <div className={styles.point_down}>
+          <img src="/point_down.png" className={styles.point_down_icon} />
+        </div>
+      </div>
+      <div className ={styles.select_amount}>
+        Select tocken and amount
+      </div>
+        <div>
+          <input className={styles.number_input_1} type="number" placeholder='0' />
+        </div>
+        <div>
+          <input className={styles.number_input_2} type="number" placeholder='0' />
+        </div>
+        <div className={styles.tocken_name_1}>
+          SOL
+        </div>
+        <div className={styles.tocken_balance_1}>
+          0 SOL
+        </div>
+        <button className={styles.bridge_to_button}>
+            Bridge to
+        </button>
+    </main>
+  );
+}
---
+++ src/app/send-page/page.tsx
@@ -0,0 +1,38 @@
+import { useRouter } from 'next/navigation'; 
+import styles from './send.module.css';
+import React from 'react'
+
+interface ButtonProps {
+  onClick: () => void;
+  Button: React.ReactNode;
+}
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.header}>
+        <h1 className={styles.title}>Send</h1>
+      </div>
+      <div className={styles.From}>
+        <h1 className={styles.From}>From</h1>
+      </div>
+      <div className={styles.To}>
+        <h1 className={styles.To}>To</h1>
+      </div>
+      <div>
+          <input className={styles.address_input} placeholder='Enter public address' />
+        </div>
+        <button className={styles.back_button}>
+          <img src="/back_button.png" className={styles.back_button_icon} />
+        </button>
+        <div className={styles.buttons}>
+            <button className={styles.cancel_button}>
+                Cancel
+            </button>
+            <button className={styles.continue_button}>
+                Continue
+            </button>
+        </div>
+    </main>
+  );
+}

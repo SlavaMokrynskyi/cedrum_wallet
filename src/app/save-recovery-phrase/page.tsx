b14541d3755dd4a351e1b094984bd973d95a1550
da2045 <vitde2009@gmail.com>
2025-08-11 01:18:38 +0300

    доробив recovery phrase

--- src/app/save-recovery-phrase/page.tsx
+++ src/app/save-recovery-phrase/page.tsx
@@ -15,6 +15,20 @@ export default function page() {
         <div className={styles.title_div}>
             <p className={styles.title_step}>Step 2 of 3</p>
         </div>
+        <div className={styles.title_big_div}>
+          <p className={styles.title_big}>Save your Secret<br/> Recovery Phrase</p>
+        </div>
+        <div className={styles.title_small_div}>
+          <p className={styles.title_small}>This is your Secret Recovery Phrase. Write it down in the correct order and keep it safe. If someone has your Secret Recovery Phrase, they can access your wallet.</p>
+        </div>
+        <div className={styles.title_small_div}>
+          <p className={styles.title_small}>Don't share it with anyone, ever.</p>
+        </div>
+        <div className={styles.later_button_div}>
+          <MyButton className={styles.later_button} href='/'>
+            <p >Continue</p>
+          </MyButton>
+        </div>
         </div>
     </main>
   )

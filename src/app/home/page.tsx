b23df0cfb3c511debfd5cdff42e487f72eadff8b
da2045 <vitde2009@gmail.com>
2025-08-14 20:21:03 +0300

    buttons home

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -15,31 +15,43 @@ export default function page() {
             <BalanceComponent/>
           </div>
           <div className={styles.buttons_row}>
-            <div className={styles.swap_div}>
-              <MyButton className={styles.swap} href='/swap-page'>
-                <img src='/swap.png' className={styles.button_icon}/>
-                <p className={styles.button_title}>Swap</p>
-              </MyButton>
-            </div>
-            <div className={styles.bridge_div}>
-              <MyButton className={styles.swap} href='/bridge-page'>
-              <img src='/bridge.png' className={styles.button_icon}/>
-              <p className={styles.button_title}>Bridge</p>
-              </MyButton>
-            </div>
-            <div className={styles.send_div}>
-              <MyButton className={styles.swap} href='/send-page'>
-              <img src='/swap.png' className={styles.button_icon}/>
-              <p className={styles.button_title}>Send</p>
-              </MyButton>
-            </div>
-            <div className={styles.send_div}>
-              <MyButton className={styles.swap} href='/receive'>
-              <img src='/recieve.png' className={styles.button_icon}/>
-              <p className={styles.button_title}>QR</p>
-              </MyButton>
-            </div>      
-          </div>
+  <div className={styles.button_box}>
+    <MyButton  className={styles.button} href='/swap-page'>
+      <div className={styles.icon_wrapper}>
+        <img src='/swap.png' alt='Swap'/>
+      </div>
+    </MyButton>
+    <p className={styles.button_title}>Swap</p>
+  </div>
+
+  <div className={styles.button_box}>
+    <MyButton className={styles.button} href='/bridge-page'>
+      <div className={styles.icon_wrapper}>
+        <img src='/bridge.png' alt='Bridge'/>
+      </div>
+    </MyButton>
+    <p className={styles.button_title}>Bridge</p>
+  </div>
+
+  <div className={styles.button_box}>
+    <MyButton  className={styles.button} href='/send-page'>
+      <div className={styles.icon_wrapper}>
+        <img src='/send.png' alt='Send'/>
+      </div>
+    </MyButton>
+    <p className={styles.button_title}>Send</p>
+  </div>
+
+  <div className={styles.button_box}>
+    <MyButton className={styles.button}  href='/receive'>
+      <div className={styles.icon_wrapper}>
+        <img src='/recieve.png' alt='QR'/>
+      </div>
+    </MyButton>
+    <p className={styles.button_title}>QR</p>
+  </div>
+</div>
+
 
           <HomeFooter/>
         </div>
